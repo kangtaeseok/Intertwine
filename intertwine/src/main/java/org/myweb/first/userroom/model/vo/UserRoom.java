cc683f52831d511d04d39e848517f517149d0999
kangtaeseok <goxotjr@naver.com>
2024-04-02 13:17:13 +0900

    태석 0402 점심시간 커밋

---
+++ intertwine/src/main/java/org/myweb/first/userroom/model/vo/UserRoom.java
@@ -0,0 +1,17 @@
+package org.myweb.first.userroom.model.vo;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class UserRoom implements Serializable {
+	private static final long serialVersionUID = -2317634193220268983L;
+	private String userId;
+	private String roomColor;
+	private String userPwd;
+	private String nickname;
+
+	
+	
+	
+	
+}
