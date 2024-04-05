d72378202d9d41fc315b83b23c496c7347a20ef2
goxot <goxot@goxot>
2024-04-05 14:31:04 +0900

    0405 점심 커밋

---
+++ intertwine/src/main/java/org/edu/intertwine/usercharacter/model/dao/UserCharacterDao.java
@@ -0,0 +1,38 @@
+package org.edu.intertwine.usercharacter.model.dao;
+
+import org.edu.intertwine.usercharacter.model.vo.UserCharacter;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository("userCharacterDao")
+public class UserCharacterDao {
+	// 마이바이스 매퍼 파일에 쿼리문 별도로 작성함
+	// root-context.xml 에 지정된 마이바티스 연결 객체를 사용함
+	@Autowired // root-context.xml 에서 생성한 객체를 자동 연결함
+	private SqlSessionTemplate sqlSessionTemplate;
+
+	public UserCharacter selectUserCharacterFirst(String userId) {
+		return sqlSessionTemplate.selectOne("userCharacterMapper.selectUserCharacterFirst", userId);
+	}
+	
+	public UserCharacter selectUserCharacter(String userId) {
+		return sqlSessionTemplate.selectOne("userCharacterMapper.selectUserCharacter", userId);
+	}
+
+	public int insertUserCharacterFirst(String userId) {
+		return sqlSessionTemplate.insert("userCharacterMapper.insertUserCharacterFirst", userId);
+	}
+
+	public int UpdateUserCharacter(String userId) {
+		return sqlSessionTemplate.insert("userCharacterMapper.insertUserCharacter", userId);
+	}
+	
+	public int insertUserCharacter(UserCharacter userCharacter) {
+		return sqlSessionTemplate.insert("userCharacterMapper.insertUserCharacter", userCharacter);
+	}
+
+	public int deleteUserCharacter(String userId) {
+		return sqlSessionTemplate.delete("userCharacterMapper.deleteUserCharacter", userId);
+	}
+}
